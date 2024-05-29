73665bd62960380d16cfa051039689c85ab23df9
Tanjid Khan Arnob <tanjidkhanarnob2000@gmail.com>
2024-05-30 00:31:06 +0600

    Done remaining works

--- gold-kinen-react/src/components/CommentList.jsx
+++ gold-kinen-react/src/components/CommentList.jsx
@@ -2,11 +2,13 @@
 import PropTypes from 'prop-types';
 const CommentList = ({ comments }) => {
   return (
-    <ul className="mt-4 pl-4 border-l border-gray-200">
+    <ul className="mt-8 pl-4 border-l border-gray-200">
       {comments.map(comment => (
         <li key={comment.id} className="mb-2">
           <p className="text-gray-800">{comment.body}</p>
-          <p className="text-sm text-gray-500">- {comment.email}</p>
+          <div className='w-[300px]'>
+          <p className="text-sm font-bold mt-2 text-transparent bg-clip-text bg-gradient-to-r from-pink-400 via-purple-600 to-indigo-500">- {comment.email}</p>
+          </div>
         </li>
       ))}
     </ul>
